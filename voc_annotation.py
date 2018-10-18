fa328a67c88dcf8350b008e675bf40737d318924
liang.gao01 <liang.gao01@gz-public-test-app114-140-30.msfinance.host>
2018-10-18 11:26:56 +0800

    2007voc生成训练需要的文件格式

--- voc_annotation.py
+++ voc_annotation.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 from os import getcwd
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets=[('2007', 'train'), ('2007', 'val')]
 
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
